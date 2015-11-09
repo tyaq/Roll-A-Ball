c5329cb94b7f01153f3e71afb69e4cc57e4f35dd
Ishtyaq Habib <ishtyaq.habib@gmail.com>
2015-11-08 20:53:59 -0500

    Added Player Movement
    Created PlayerController script.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	/// <summary>
+	/// Movement sensitivity multiplier.
+	/// </summary>
+	public float speed;
+	
+	private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	//Place movement here
+	void Update () {
+	
+	}
+
+	//Called before performing any physics calculations
+	//Place physics here
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		
+		rb.AddForce (movement * speed);
+	}
+}
