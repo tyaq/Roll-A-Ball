0888af8a9fc9e13ed47568d8cc3bc7d59bf8bcfc
Ishtyaq Habib <ishtyaq.habib@gmail.com>
2015-11-08 21:50:24 -0500

    Added 3rd Person Camera Direction
    Created PlayerController script. Camera follows the player around.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject player;
+
+	/// <summary>
+	/// The offset vector for a follow camra that is a fixed distance away from the player.
+	/// </summary>
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - player.transform.position;
+	}
+	
+	// LateUpdate is called once per frame after update is done processing
+	void LateUpdate () {
+		transform.position = player.transform.position + offset;
+	}
+}
